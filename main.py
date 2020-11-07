1664efaf4ec6bed004483e8ffc7867ad672e42f8
Marek.Hering <marek.hering99@gmail.com>
2020-11-07 19:49:26 +0100

    add basic read/write functions

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+import random
+
+
+def clear_file(filename):
+    with open(filename, 'w') as _:
+        pass
+
+
+def save_to_file(filename, data):
+    with open(filename, 'a') as file:
+        file.write(str(data) + '\n')
+
+
+def load_from_file(left_bounds, right_bounds):
+    with open(filename, 'r') as file:
+        result = []
+        for index, line in enumerate(file.readlines()):
+            if left_bounds <= index <= right_bounds:
+                result.append(int(line))
+    return result
+
 
 if __name__ == '__main__':
-    print("Hello world")
+    filename = "temp.txt"
+
+    clear_file(filename)
+
+    random_number = random.randint(0, 10 ** 20)
+    print(random_number)
+    save_to_file(filename, random_number)
+
+    random_number = random.randint(0, 10 ** 20)
+    print(random_number)
+    save_to_file(filename, random_number)
+
+    print(load_from_file(0, 1))
