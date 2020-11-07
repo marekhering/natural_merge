83d8d4373d2ded9c1c17ff8be52d4b549deef7f3
Marek.Hering <marek.hering99@gmail.com>
2020-11-07 21:11:59 +0100

    add read records functions

--- sorting/algorithm.py
+++ sorting/algorithm.py
@@ -1,5 +1,26 @@
+from natural_merge.file_operations import read_from_file
+from natural_merge.structures import Record
 
 
 def sort_file(file_dir):
 
-    record = read_record
+    stop_clause = False
+    record_index = 0
+
+    create_tapes()
+
+    while stop_clause is False:
+
+        record = read_record(file_dir, record_index)
+
+
+def read_record(file_dir, index):
+    # Temporary function
+    # TODO Block read
+    value = read_from_file(file_dir, index, index)
+    value = int(value[0])
+    return Record(value)
+
+
+def create_tapes():
+    pass
