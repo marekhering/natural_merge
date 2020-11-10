d3970d96b0a3e68d68ca9499907af9bf51366108
Marek.Hering <marek.hering99@gmail.com>
2020-11-10 22:25:05 +0100

    remove file read lines function

--- file_operations/file_operations.py
+++ file_operations/file_operations.py
@@ -10,15 +10,6 @@ def save_to_file(file_dir, data):
         file.write(str(data) + '\n')
 
 
-def read_from_file(file_dir, left_bounds, right_bounds):
-    with open(file_dir, 'r') as file:
-        result = []
-        for index, line in enumerate(file.readlines()):
-            if left_bounds <= index <= right_bounds:
-                result.append(line)
-    return result
-
-
 def print_file(file_dir):
     with open(file_dir, 'r') as file:
         for line in file.readlines():
