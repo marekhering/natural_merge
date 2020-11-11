9116b09efaaa01d9588d3ce20f929de11d331724
Marek.Hering <marek.hering99@gmail.com>
2020-11-11 12:09:52 +0100

    automation algorithm

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ if __name__ == '__main__':
 
     file_dir = "data/numbers.txt"
     numbers_amount = 20
+
+
+
     # generate_data(file_dir, numbers_amount)
-    # print_file(file_dir)
 
     sort_file(file_dir)
 
---
+++ sorting/structures/record_list.py
@@ -0,0 +1,41 @@
+
+
+class RecordList(list):
+    def __init__(self, size):
+        super(RecordList).__init__()
+        self.extend([None] * size)
+
+    def get_next_record(self, prev_record):
+        if prev_record is None:
+            next_record, next_record_index = self.minimum_value_and_index()
+            if next_record_index is not None:
+                self[next_record_index] = None
+            return next_record
+
+        next_record, next_record_index = self.minimum_value_and_index(higher_than=prev_record)
+
+        if next_record is None:
+            next_record, next_record_index = self.minimum_value_and_index()
+
+        if next_record_index is not None:
+            self[next_record_index] = None
+
+        return next_record
+
+    def none_full(self):
+        for record in self:
+            if record is not None:
+                return False
+        return True
+
+    def minimum_value_and_index(self, higher_than=None):
+        min_val = None
+        min_ind = None
+
+        for index, record in enumerate(self):
+            if record is not None and (min_val is None or record < min_val):
+                if higher_than is None or record > higher_than:
+                    min_val = record
+                    min_ind = index
+
+        return min_val, min_ind
